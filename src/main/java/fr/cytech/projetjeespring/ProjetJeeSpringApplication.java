7b4c9e570b14966fc579d92b2f5aac0d521bc271
fleefie <mail@fleefie.fr>
2025-11-28 14:21:37 +0100

    Classe principale

---
+++ src/main/java/fr/cytech/projetjeespring/ProjetJeeSpringApplication.java
@@ -0,0 +1,12 @@
+package fr.cytech.projetjeespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetJeeSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetJeeSpringApplication.class, args);
+    }
+}
