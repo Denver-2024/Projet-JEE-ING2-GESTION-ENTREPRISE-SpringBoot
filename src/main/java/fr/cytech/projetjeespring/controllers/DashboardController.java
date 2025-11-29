3ace9c549a799ab657284dbcf8333f6553216411
fleefie <mail@fleefie.fr>
2025-11-29 17:58:22 +0100

    Page index

---
+++ src/main/java/fr/cytech/projetjeespring/controllers/DashboardController.java
@@ -0,0 +1,12 @@
+package fr.cytech.projetjeespring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DashboardController {
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+}
