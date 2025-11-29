5d98f5405572f734fd6fcafb1bedba38ebefdcb1
fleefie <mail@fleefie.fr>
2025-11-29 17:25:37 +0100

    Refactor: Utilisation de @Data au lieu de @getter et @setter partout

--- src/main/java/fr/cytech/projetjeespring/entities/Department.java
+++ src/main/java/fr/cytech/projetjeespring/entities/Department.java
@@ -1,44 +1,34 @@
 package fr.cytech.projetjeespring.entities;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 import java.util.ArrayList;
 
 @Entity
 @Table(name = "departments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Department {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter
-    @Setter
     private Integer id;
 
     @Column(nullable = false)
-    @Getter
-    @Setter
     private String name;
 
-    @Getter
-    @Setter
     private String description;
 
     @OneToOne(optional = false, fetch = FetchType.LAZY)
     @JoinColumn(name = "head_employee_id", nullable = false)
-    @Getter
-    @Setter
     private Employee head;
 
     @OneToMany(mappedBy = "department", fetch = FetchType.LAZY)
-    @Getter
-    @Setter
     private List<Employee> employees = new ArrayList<>();
 
     @OneToMany(mappedBy = "department", fetch = FetchType.LAZY)
-    @Getter
-    @Setter
     private List<Project> projects = new ArrayList<>();
 }
--- src/main/java/fr/cytech/projetjeespring/entities/Project.java
+++ src/main/java/fr/cytech/projetjeespring/entities/Project.java
@@ -2,37 +2,29 @@ package fr.cytech.projetjeespring.entities;
 
 import fr.cytech.projetjeespring.enums.ProjectState;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "projects")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Project {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter
-    @Setter
     private Integer id;
 
     @Column(nullable = false)
-    @Getter
-    @Setter
     private String name;
 
-    @Getter
-    @Setter
     private String description;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    @Getter
-    @Setter
     private ProjectState state;
 
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
     @JoinColumn(name = "department_id", nullable = false)
-    @Getter
-    @Setter
     private Department department;
 }
